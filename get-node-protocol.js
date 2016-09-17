dd2ab3c0428ede261015d7464570c87feb95cc0e
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-09-17 15:53:50 -0700

    Add script to get Node protocol	.

---
+++ get-node-protocol.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var fs = require('fs')
+var request = require('request-promise')
+
+var fetchProtocol = () => {
+  return new Promise((resolve, reject) => {
+    var url = 'https://chromium.googlesource.com/v8/v8/+/master/src/inspector/js_protocol.json?format=TEXT'
+    return request(url).then(body => {
+      var protocol = JSON.parse(Buffer.from(body, 'base64').toString('utf8'))
+      resolve(protocol)
+    })
+  })
+}
+
+fetchProtocol().then(protocol => {
+  fs.writeFile('protocols/node/protocol.json', JSON.stringify(protocol, null, 2), function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Node Protocol JSON file generated')
+    }
+  })
+}).catch(err => {
+  console.log('err', err)
+})
